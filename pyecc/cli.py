5bf2361ecb29bb816acb1546abf9f628d61d82c7
Sauci <guillaumesottas@gmail.com>
2019-10-28 18:38:04 +0100

    Remove unused CLI option

--- pyecc/cli.py
+++ pyecc/cli.py
@@ -13,20 +13,12 @@ def main():
     parser = argparse.ArgumentParser(prog='pya2l', description='python command line utility for TI\'s ECC.')
     parser.add_argument('input_file', help='input file path (*.elf)')
     parser.add_argument('output_file', type=argparse.FileType('wb'), help='output file path (*.bin)')
-    parser.add_argument('-O',
-                        dest='output_format',
-                        metavar='output format',
-                        action='store',
-                        default='binary',
-                        nargs='?',
-                        help='output file format')
 
     args = parser.parse_args()
 
     ecc = ECCGen(args.input_file)
 
-    if args.output_format == 'binary':
-        args.output_file.write(ecc.get_ecc_from_elf())
+    args.output_file.write(ecc.get_ecc_from_elf())
 
 
 if __name__ == '__main__':
