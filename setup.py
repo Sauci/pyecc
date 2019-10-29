f44ce309a8a462ff9ccd3c8ca6c7b35815b7c7ce
Sauci <guillaumesottas@gmail.com>
2019-10-29 16:28:55 +0100

    Add setup.py file

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='pyecc',
+    version='0.1.0',
+    packages=['pyecc'],
+    url='https://github.com/Sauci/pyecc',
+    license='BBSD',
+    author='Guillaume Sottas',
+    author_email='guillaumesottas@gmail.com',
+    description='This tool generates the ECC data to be programmed into flash ECC of TMS570 Platform Series microcontrollers',
+    entry_points={
+        'console_scripts': ['pyecc=pyecc:main']
+    },
+    dependency_links=['http://github.com/Sauci/pyelf/tarball/master#egg=pyelf-0.1.0'],
+    install_requires=['PyYAML==5.1.2']
+)
