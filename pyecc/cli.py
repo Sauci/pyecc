d03d0dc89fc7321f465b45e5bbd10201ff5a76d2
Sauci <guillaumesottas@gmail.com>
2019-11-04 07:15:25 +0100

    Specify supported file formats in CLI help message

--- pyecc/cli.py
+++ pyecc/cli.py
@@ -17,8 +17,8 @@ def main():
                         type=str,
                         choices=[k for k in get_configuration().keys() if k not in ('shared_data',)],
                         help='target device')
-    parser.add_argument('input_file', help='input file path')
-    parser.add_argument('output_file', type=argparse.FileType('wb'), help='output file path')
+    parser.add_argument('input_file', help='input file path (*.bin, *.elf, *.srec)')
+    parser.add_argument('output_file', type=argparse.FileType('wb'), help='output file path (*.bin)')
     parser.add_argument('-endianness', type=str, default='little', choices=('big', 'little'))
     parser.add_argument('--version', action='version', version='%(prog)s {}'.format('0.1.0'))
 
